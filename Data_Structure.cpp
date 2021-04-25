def2b6d21398e1b21eaafd4a684d495113bae2c6
Ze17-gif <karljoseph.aguilar@g.batstate-u.edu.ph>
2021-04-25 23:18:58 +0800

    Day 1
    Start doing things dummy

--- Data_Structure.cpp
+++ Data_Structure.cpp
@@ -1,11 +1,45 @@
-#include<iostream> 
+#include<iostream>
 
 using namespace std;
+
+
 //// Parking management system
 
 ///// new
 
+////////  Functions
+
+////////  Display Welcome 
+
+void WCP(){
+    cout<<"\n\t\t\t WELCOME TO \n\n";
+    cout<<"\n\t\tPARKING MANAGEMENT SYSTEM\t\t\n";
+    cout <<"\n\n\t\tPress Enter to Continue...\n";
+    cin.ignore();
+    system("CLS");
+}
+
+////////  Display Menu
+
+void Menu(){
+    int option;
+    cout<<setw(25);
+    cout<<"\t\t\t\tMENU\n\n";
+    cout<<"\t\t1. PARK \n";
+    cout<<"\t\t2. UNPARK \n";
+    cout<<"\t\t3. SEARCH \n";
+    cout<<"\t\t4. VIEW TOTAL VEHICLES \n";
+    cout<<"\t\t5. VIEW TOTAL SALES \n\n";
+    cout<<"\t\tSELECT OPTION:";
+    cin>> option ;
+    if option 
+    }
+
+
 int main(){
-    cout<<"Hello world";
+    WCP();
+    Menu();
+    
 return 0;
 }
+

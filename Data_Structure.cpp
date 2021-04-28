19cabb79e6d34507a8a1cd85550b9950ca64cb8e
Ze17-gif <karljoseph.aguilar@g.batstate-u.edu.ph>
2021-04-28 21:17:51 +0800

    Yt source
    https://www.youtube.com/watch?v=5kqJlkQCukk

--- Data_Structure.cpp
+++ Data_Structure.cpp
@@ -1,36 +1,106 @@
 #include<iostream>
 #include<vector>
+#include<iomanip> // for input output manipulation
 
 using namespace std;
 
 ///////// Functions
 
-void clear() // For clearing screen
-{
+void clear(){ // For clearing screen
     system("cls");
 }
+
+int getChoice(){
+
+    int choice = 0;
+    cout<< "*******************************************************************************" <<endl
+    << "\t\t\tPARKING MANAGEMENT SYSTEM " <<endl
+    << "*******************************************************************************\n" << endl
+    << "\t\t\t1. PARK" << endl
+    << "\t\t\t2. UNPARK" << endl
+    << "\t\t\t3. TOTAL VEHICLE" << endl
+    << "\t\t\t4. TOTAL SALES" << endl
+    << "\t\t\t5. SEARCH VEHICLE" << endl
+    << "\t\t\t6. Exit SYSTEM" << endl
+    << "_______________________________________________________________________________" << endl
+    << "Enter choice : ";
+    cin>> setw(1)>> choice;
+
+    // Error check 
+    while (!cin.good()){
+        //Print Error
+        cout << "ERROR: Invalid Input, try again...\n";
+        
+
+        //  clear stream
+        cin.clear();
+        cin.ignore(INT_MAX, '\n');
+
+        //Get input again..
+        cout << "Enter Choice: ";
+        cin>> setw(1)>> choice;
+
+    }
+    //  clear stream
+    cin.clear();
+    cin.ignore(INT_MAX, '\n');
+
+    return choice;
+}
+
 
 ////////  Main
 
 int main(){
 
-    int choice;
+    int choice = -1;
     char ans;
+    
+    choice = getChoice();
 
+    while   (choice > 0 ){ // menu
+        switch (choice){
+            case 1:
+                clear();
+                cout<< " this is 1";
+                break;
+            case 2:
+                clear();
+                cout<<"this is 2";               
+                break;
+            case 3:
+                clear();
+                cout<<"this is 3";
+                break;                
+            case 4:
+                clear();
+                cout<<"this is 4";
+                break;
+            case 5:
+                clear();
+                cout<<"this is 5";
+                break;
+            case 6:
+                exit(0);
+                break;
+            default:
+                clear();
+                cout<< " Invalid input try again";
+                break;
+        }
 
-        cout<< "*******************************************************************************" <<endl
-            << "\t\t\tPARKING MANAGEMENT SYSTEM " <<endl
-            << "*******************************************************************************\n" << endl
-            << "\t\t\t1. PARK" << endl
-            << "\t\t\t2. UNPARK" << endl
-            << "\t\t\t3. TOTAL VEHICLE" << endl
-            << "\t\t\t4. TOTAL SALES" << endl
-            << "\t\t\t5. SEARCH VEHICLE" << endl
-            << "\t\t\t6. Exit SYSTEM" << endl
-            << "_______________________________________________________________________________" << endl
-            << "Enter choice : ";
-        cin >> choice;
+        cout << "\nDo you want to continue, press y/n : " << endl;
+        cin >> ans;
+        if (ans == 'n'){
+            break;
+        }
+        else{
+            clear();
+            getChoice();
+        }
+    }
 
+    system("pause");
     return 0;
 }
 
